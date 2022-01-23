c654af812ab79121ece15a811cf29e57164de321
Platinum392 <98253282+Platinum392@users.noreply.github.com>
2022-01-23 16:32:48 +0800

    Update index.js

--- Script/index.js
+++ Script/index.js
@@ -102,15 +102,15 @@ var _gameBBList = [],
 
 function gameInit() {
     createjs.Sound.registerSound({
-        src: "./static/music/err.mp3",
+        src: "./Script/music/err.mp3",
         id: "err"
     });
     createjs.Sound.registerSound({
-        src: "./static/music/end.mp3",
+        src: "./Script/music/end.mp3",
         id: "end"
     });
     createjs.Sound.registerSound({
-        src: "./static/music/tap.mp3",
+        src: "./Script/music/tap.mp3",
         id: "tap"
     });
     gameRestart();
